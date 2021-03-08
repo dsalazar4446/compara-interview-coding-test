08bde67b7a8678cf40732f656f686cd94f75f7b1
Daniel Salazar <dsalazar446@gmail.com>
2021-03-08 04:52:37 -0400

    include classes and txt example from original project

---
+++ src/classes/product.class.ts
@@ -0,0 +1,10 @@
+export class Product {
+    name: string;
+    sellIn: number
+    price: number
+    constructor(name, sellIn, price) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.price = price;
+    }
+}
